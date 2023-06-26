97ae7312b80f8ab2194896dab982ec50c8d43d89
issuebombom <issuebombom@gmail.com>
2023-06-26 20:46:56 +0900

    feat: user entrance things
    회원가입, 로그인, 로그아웃이 구현되어 있습니다.
    
    [세부내역 정리]
    회원가입 관련
    - 비밀번호는 해시값으로 변경되어 저장합니다.
    
    로그인 관련
    - 로그인 시 엑세스토큰과 리프레시 토큰을 생성합니다.
    - 엑세스 토큰은 발급 시 쿠키(accessCookie)에 저장됩니다.
    - 리프레시토큰은 생성 후 엑세스 토큰 생성에 재료로 사용되고, 또한 유저 데이터베이스에 저장됩니다.
    
    로그아웃 관련
    - 작동 시 본인의 크롬에 저장된 accessCookie라는 쿠키가 삭제됩니다.
    - :userId를 params로 받는 이유는 "XXX"님이 삭제되었습니다. 라는 메시지를 res하기위함입니다.

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const express = require('express');
-const Users = require('./models/users');
+const { verifyAccessToken, replaceAccessToken } = require('./middleware/auth.middleware');
+
+// 각 라우트 파일 이름이 정해지면 변경해서 활성화하기
+const usersRouter = require('./routes/users.router');
+// const postsRouter = require('./routes/posts.router');
+// const commentsRouter = require('./routes/comments.router');
 
 const HOST = '127.0.0.1';
 const PORT = 3000;
@@ -10,23 +15,10 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser()); // npm i cookie-parser
 
-// 각 라우트 파일 이름이 정해지면 변경해서 활성화하기
-// const usersRouter = require('./routes/users.router');
-// const authorizationRouter = require('./routes/authorization.router');
-// const postsRouter = require('./routes/posts.router');
-
-// app.use('/users', usersRouter);
-// app.use('/auth', authorizationRouter);
-// app.use('/posts', postsRouter);
-
-// 예시
-app.get('/', async (req, res) => {
-  try {
-    const findUsers = await Users.findByPk(1);
-  } catch {
-    res.send({ msg: '데이터가 없음' });
-  }
-});
+app.post('/signup', usersRouter.signup);
+app.post('/login', usersRouter.login);
+app.get('/logout/:userId', usersRouter.logout);
+// app.get('/users/:userId', verifyAccessToken, replaceAccessToken, usersRouter.getUser); // 미들웨어 테스트용
 
 app.listen(PORT, HOST, () => {
   console.log('Server is listening...', PORT);
