acab02a96affdffd6736a74959f7eac6d18c0d06
issuebombom <issuebombom@gmail.com>
2023-06-26 16:13:05 +0900

    feat: init project

---
+++ app.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const Users = require('./models/users');
+
+const HOST = '127.0.0.1';
+const PORT = 3000;
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser()); // npm i cookie-parser
+
+// 각 라우트 파일 이름이 정해지면 변경해서 활성화하기
+// const usersRouter = require('./routes/users.router');
+// const authorizationRouter = require('./routes/authorization.router');
+// const postsRouter = require('./routes/posts.router');
+
+// app.use('/users', usersRouter);
+// app.use('/auth', authorizationRouter);
+// app.use('/posts', postsRouter);
+
+// 예시
+app.get('/', async (req, res) => {
+  try {
+    const findUsers = await Users.findByPk(1);
+  } catch {
+    res.send({ msg: '데이터가 없음' });
+  }
+});
+
+app.listen(PORT, HOST, () => {
+  console.log('Server is listening...', PORT);
+});
---
+++ migrations/20230626061506-create-posts.js
@@ -0,0 +1,51 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Posts', {
+      postId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      content: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      category: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      likes: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      img: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Posts');
+  },
+};
