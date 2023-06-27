9da4337ef7ad7a71e2d0f6bd88537ae10c6bd5a3
Lee SangHun <mysh1105@gmail.com>
2023-06-27 15:32:22 +0900

    test

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const { verifyAccessToken, replaceAccessToken } = require('./middleware/auth.mid
 
 // 각 라우트 파일 이름이 정해지면 변경해서 활성화하기
 const usersRouter = require('./routes/users.router');
-// const postsRouter = require('./routes/posts.router');
+const postsRouter = require('./routes/posts.router');
 // const commentsRouter = require('./routes/comments.router');
 
 const HOST = '127.0.0.1';
