88d7e0f9abf981594b002bb09eb13654af72e4de
yooonzero <playlist808@kakao.com>
2023-06-27 14:13:44 +0900

    after merge

--- app.js
+++ app.js
@@ -16,6 +16,15 @@ app.use(express.json());
 app.use(cookieParser()); // npm i cookie-parser
 
 // 각 라우트 파일 이름이 정해지면 변경해서 활성화하기
+// const usersRouter = require('./routes/users.router');
+// const authorizationRouter = require('./routes/authorization.router');
+// const postsRouter = require('./routes/posts.router');
+const mypageRouter = require('./routes/mypage.route.js');
+
+// app.use('/users', usersRouter);
+// app.use('/auth', authorizationRouter);
+// app.use('/posts', postsRouter);
+app.use('/mypage', mypageRouter);
 // 예시
 app.post('/signup', usersRouter.signup);
 app.post('/login', usersRouter.login);
---
+++ routes/mypage.route.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const router = express.Router();
+const authMiddleware = require('../middleware/auth.middleware.js');
+const Users = require('../models/users.js');
+const Posts = require('../models/posts.js');
+const Comments = require('../models/comments.js');
+const { Mypage } = require('../models/mypage.js');
+const { Op } = require('sequelize');
+
+// 유저 정보 조회
+router.get('/mypage/:userId', authMiddleware, async (req, res) => {
+  const { userId } = req.params;
+  const user = await Users.findByPk(userId);
+  const writtenPosts = await Posts.findAll({ where: { userId: userId } });
+  const writtenComments = await Comments.findAll({ where: { userId: userId } });
+
+  try {
+    const userData = {
+      userDetail: user.map((a) => {
+        return {
+          userId: a.userId,
+          nickname: a.nickname,
+          email: a.email,
+          password: a.password,
+          category: a.category,
+        };
+      }),
+    };
+    const writtenPostsData = {
+      writtenPosts: writtenPosts.map((b) => {
+        return {
+          postId: b.postId,
+          title: b.title,
+          content: b.content,
+        };
+      }),
+    };
+    const writtenCommentsData = {
+      writtenComments: writtenComments.map((c) => {
+        return {
+          commentId: c.commentId,
+          content: c.content,
+        };
+      }),
+    };
+    res.status(200).json(userData, writtenPostsData, writtenCommentsData);
+  } catch {
+    //
+  }
+});
+
+// 유저 정보 수정
+router.put('/mypage/:userId', authMiddleware, async (req, res) => {
+  const { userId } = req.params;
+  const { nickname, email, password, category } = req.body;
+  try {
+    if (nickname === '' || nickname === undefined) {
+      return res.status(412).json({ errorMessage: '닉네임의 형식이 올바르지 않습니다.' });
+    }
+    if (email === '' || email === undefined) {
+      return res.status(412).json({ errorMessage: '이메일의 형식이 올바르지 않습니다.' });
+    }
+    if (password === '' || password === undefined) {
+      return res.status(412).json({ errorMessage: '비밀번호의 형식이 올바르지 않습니다.' });
+    }
+    if (category === '' || category === undefined) {
+      return res.status(412).json({ errorMessage: '카테고리의 형식이 올바르지 않습니다.' });
+    }
+    await Users.updateOne(
+      { userId: userId },
+      { $set: { nickname, email, password, category } }
+    ).catch((err) => {
+      res.status(401).json({ errorMessage: '유저정보가 정상적으로 수정되지 않았습니다.' });
+    });
+    res.status(201).json({ message: '유저정보를 성공적으로 수정하였습니다.' });
+  } catch (error) {
+    res.status(400).json({ errorMessage: '유저정보 수정에 실패하였습니다.' });
+  }
+});
+
+module.exports = router;
