929f14ae929654e2f02cc43de00da0b316d4a868
issuebombom <issuebombom@gmail.com>
2023-06-27 20:09:58 +0900

    chore: minor

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const express = require('express');
-const { verifyAccessToken, replaceAccessToken } = require('./middleware/auth.middleware');
-const postsRouter = require('./routes/posts.router');
+
 const mypageRouter = require('./routes/mypage.route');
 const usersRouter = require('./routes/users.router');
-
 const postsRouter = require('./routes/posts.router');
 // const commentsRouter = require('./routes/comments.router');
-
 
 const HOST = '127.0.0.1';
 const PORT = 3000;
@@ -17,14 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser()); // npm i cookie-parser
 
-
-app.post('/signup', usersRouter.signup);
-app.post('/login', usersRouter.login);
-app.get('/logout/:userId', usersRouter.logout);
-// app.get('/users/:userId', verifyAccessToken, replaceAccessToken, usersRouter.getUser); // 미들웨어 테스트용
-app.use('/', [postsRouter, mypageRouter]);
-app.use('/', [usersRouter, postsRouter]);
-
+app.use('/', [usersRouter, postsRouter, mypageRouter]);
 
 app.listen(PORT, HOST, () => {
   console.log('Server is listening...', PORT);
