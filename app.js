2ecc12c9638a57c5761488218d7c8059b6b1b463
issuebombom <79882498+issuebombom@users.noreply.github.com>
2023-06-27 19:32:22 +0900

    Revert "fix: route handler, authMiddleware"

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const { verifyAccessToken, replaceAccessToken } = require('./middleware/auth.mid
 
 // 각 라우트 파일 이름이 정해지면 변경해서 활성화하기
 const usersRouter = require('./routes/users.router');
-const postsRouter = require('./routes/posts.router');
+// const postsRouter = require('./routes/posts.router');
 // const commentsRouter = require('./routes/comments.router');
 
 const HOST = '127.0.0.1';
@@ -15,7 +15,13 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser()); // npm i cookie-parser
 
-app.use('/', [usersRouter, postsRouter]);
+// 각 라우트 파일 이름이 정해지면 변경해서 활성화하기
+// 예시
+app.post('/signup', usersRouter.signup);
+app.post('/login', usersRouter.login);
+app.get('/logout/:userId', usersRouter.logout);
+// app.get('/users/:userId', verifyAccessToken, replaceAccessToken, usersRouter.getUser); // 미들웨어 테스트용
+app.use('/', [postsRouter]);
 
 app.listen(PORT, HOST, () => {
   console.log('Server is listening...', PORT);
