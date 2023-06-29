316716bb4e7be4febfa4e8fb6fd90294b3361ff8
issuebombom <issuebombom@gmail.com>
2023-06-29 13:23:48 +0900

    fix: logout except userId params
    - 로그 아웃 기능에 검증 미들웨어(쿠키 유무, 유효성검사)를 추가함
    - 로그아웃에 userId params는 불필요하다 판단하여 제거

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,6 +1,7 @@
-const { Users, Categories } = require('../models');
+const { Users } = require('../models');
 const errors = require('../assets/errors');
 const authMiddleware = require('../middleware/auth.middleware');
+const { verifyAccessToken } = require('../middleware/auth.middleware');
 const { Op } = require('sequelize');
 const bcrypt = require('bcrypt');
 const express = require('express');
@@ -21,7 +22,7 @@ router.post('/signup', async (req, res) => {
       // 닉네임 고유값에 대한 검증을 합니다.
       if (user.nickname === nickname)
         return res.status(errors.existUser.status).send({ msg: errors.existUser.msg });
-  
+
       // 이메일 고유값에 대한 검증을 합니다.
       if (user.email === email)
         return res.status(errors.existEmail.status).send({ msg: errors.existEmail.msg });
@@ -36,15 +37,16 @@ router.post('/signup', async (req, res) => {
     re = new RegExp(/^[a-zA-Z0-9]+$/);
     if (!re.test(nickname))
       return res.status(errors.validNickname.status).send({ msg: errors.validNickname.msg });
-    
+
     // 패스워드 양식 검증
     re = new RegExp(/^[a-zA-Z0-9!@#$%^&*()]+$/);
-    if (!re.test(password)) return res.status(errors.validPassword.status).send({ msg: errors.validPassword.msg });
+    if (!re.test(password))
+      return res.status(errors.validPassword.status).send({ msg: errors.validPassword.msg });
 
     // 패스워드와 패스워드 확인이 일치하는지 검증
     if (password !== confirm)
       return res.status(errors.passwordDiff.status).send({ msg: errors.passwordDiff.msg });
-    
+
     // 닉네임 패턴 비밀번호 적용 유무를 검증
     re = new RegExp(nickname, 'i');
     if (re.test(password))
@@ -94,7 +96,12 @@ router.post('/login', async (req, res) => {
 
     // 리프레시 토큰을 데이터베이스에 저장
     const refreshToken = authMiddleware.getRefreshToken(user.nickname, user.userId); // 수정 예정
-    const accessToken = authMiddleware.getAccessToken(user.nickname, user.userId, user.interest, refreshToken);
+    const accessToken = authMiddleware.getAccessToken(
+      user.nickname,
+      user.userId,
+      user.interest,
+      refreshToken
+    );
 
     const update = { refreshToken };
     await Users.update(update, { where: { userId: user.userId } });
@@ -121,20 +128,9 @@ router.post('/login', async (req, res) => {
 });
 
 // 로그 아웃
-router.get('/logout/:userId', async (req, res) => {
-  const userId = req.params.userId;
-  const cookies = req.cookies;
-
+router.get('/logout', verifyAccessToken, async (req, res) => {
   try {
-    // 데이터베이스에서 유저 정보 조회
-    const user = await Users.findByPk(userId);
-    if (!user) return res.status(errors.notUser.status).send({ msg: errors.notUser.msg });
-
-    // 쿠키가 없는 경우
-    if (!cookies?.accessCookie)
-      return res.status(errors.noCookie.status).send({ msg: errors.noCookie.msg });
-
-    // 엑세스 쿠키 삭제
+    const user = res.locals.user;
     res.clearCookie('accessCookie');
     res.send({ message: `${user.nickname}님이 로그아웃 되었습니다.` });
   } catch {
