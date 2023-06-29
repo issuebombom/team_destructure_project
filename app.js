5821b9df9b082b6ee6b1ca47a9399398273b03eb
issuebombom <issuebombom@gmail.com>
2023-06-29 21:17:53 +0900

    chore: minor

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const express = require('express');
-const path = require('path')
+const path = require('path');
 
 const usersRouter = require('./routes/users.router');
 const postsRouter = require('./routes/posts.router');
@@ -9,6 +9,7 @@ const uploadRouter = require('./routes/upload.router');
 const mypageRouter = require('./routes/mypage.router');
 const commentsRouter = require('./routes/comments.router');
 const likeRouter = require('./routes/likes.router');
+const mainRouter = require('./routes/main.router');
 
 const HOST = '127.0.0.1';
 const PORT = 3000;
@@ -37,7 +38,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser()); // npm i cookie-parser
 
-app.use('/', [usersRouter, postsRouter, mypageRouter, uploadRouter, commentsRouter, likeRouter]);
+app.use('/', [
+  usersRouter,
+  postsRouter,
+  mypageRouter,
+  uploadRouter,
+  commentsRouter,
+  likeRouter,
+  mainRouter,
+]);
 
 app.listen(PORT, HOST, () => {
   console.log('Server is listening...', PORT);
